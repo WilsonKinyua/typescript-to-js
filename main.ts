0f9b52dfe75dc49ba9fd52b345df1b8814dea78c
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-07-17 14:47:12 +0300

    typescript

---
+++ main.ts
@@ -0,0 +1,30 @@
+// function doSomething() {
+//     for(let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+//
+// doSomething();
+
+// let a: number;
+// let b: string;
+// let c: boolean;
+// let d: any;
+// let e: number[] = [1,2,3,4,5];
+// const colorRed = 0;
+// const  colorBlue = 1;
+// const  colorYellow = 2;
+//
+// enum color {
+//     Red,
+//     Blue,
+//     Green
+// }
+// let backgroundColor = color.Green;
+
+
+let message;
+message = 'abc';
+let endsWithC = message.endsWith('c');
+
