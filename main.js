ccee3cf6a4faec72711e3c4fed6bb1e9be334bb4
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-07-27 19:53:37 +0300

    typescript data

--- main.js
+++ main.js
@@ -1,63 +1,75 @@
-// function doSomething() {
-//     for(let i = 0; i < 5; i++) {
-//         console.log(i);
-//     }
-//     console.log('Finally: ' + i);
-// }
-//
-// doSomething();
-// let a: number;
-// let b: string;
-// let c: boolean;
-// let d: any;
-// let e: number[] = [1,2,3,4,5];
-// const colorRed = 0;
-// const  colorBlue = 1;
-// const  colorYellow = 2;
-//
-// enum color {
-//     Red,
-//     Blue,
-//     Green
-// }
-// let backgroundColor = color.Green;
-// let message;
-// message = 'abc';
-// let endsWithC = (<string>message).endsWith('c');
-// let alternative = (message as string).endsWith('c');
-// //=============================normal function
-// let log = function (message) {
-//     console.log(message);
-// }
-//
-// //=============================arrow function
-// let doLog = (message) => {
-//     console.log(message);
-// }
-// interface Point {
-//     x: number,
-//     y: number
-// }
-// let drawPoint = (point: Point) => {
-//
-// }
-//
-// drawPoint({
-//     x: 1,
-//     y: 3
-// });
+"use strict";
+exports.__esModule = true;
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3, 4, 5];
+var colorRed = 0;
+var colorBlue = 1;
+var colorYellow = 2;
+var color;
+(function (color) {
+    color[color["Red"] = 0] = "Red";
+    color[color["Blue"] = 1] = "Blue";
+    color[color["Green"] = 2] = "Green";
+})(color || (color = {}));
+var backgroundColor = color.Green;
+var message;
+message = 'abc';
+var endsWithC = message.endsWith('c');
+var alternative = message.endsWith('c');
+//=============================normal function
+var log = function (message) {
+    console.log(message);
+};
+//=============================arrow function
+var doLog = function (message) {
+    console.log(message);
+};
+var drawPoint = function (point) {
+};
+drawPoint({
+    x: 1,
+    y: 3
+});
 var Point = /** @class */ (function () {
-    function Point() {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
     }
     Point.prototype.draw = function () {
         console.log('X: ' + this.x, 'Y: ' + this.y);
     };
-    Point.prototype.getDistance = function (another) {
-        //...
-    };
+    Object.defineProperty(Point.prototype, "X", {
+        get: function () {
+            return this.x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error('Value cannot be less than 0');
+            this.x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Point;
 }());
-var point = new Point();
-point.y = 1;
-point.x = 2;
+var point = new Point(1, 2);
+var x = point.X;
+point.X = 10;
 point.draw();
+var point = new Point(1, 2);
+point.draw();
+//===============The facebook functionality quiz =========
+var like_component_1 = require("./like.component");
+var component = new like_component_1.LikeComponent(10, true);
+var x = component.onClick();
+// console.log(x);
--- main.ts
+++ main.ts
@@ -1,72 +1,88 @@
-// function doSomething() {
-//     for(let i = 0; i < 5; i++) {
-//         console.log(i);
-//     }
-//     console.log('Finally: ' + i);
-// }
-//
-// doSomething();
+function doSomething() {
+    for(let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
 
-// let a: number;
-// let b: string;
-// let c: boolean;
-// let d: any;
-// let e: number[] = [1,2,3,4,5];
-// const colorRed = 0;
-// const  colorBlue = 1;
-// const  colorYellow = 2;
-//
-// enum color {
-//     Red,
-//     Blue,
-//     Green
-// }
-// let backgroundColor = color.Green;
+doSomething();
 
+let a: number;
+let b: string;
+let c: boolean;
+let d: any;
+let e: number[] = [1,2,3,4,5];
+const colorRed = 0;
+const  colorBlue = 1;
+const  colorYellow = 2;
 
-// let message;
-// message = 'abc';
-// let endsWithC = (<string>message).endsWith('c');
-// let alternative = (message as string).endsWith('c');
+enum color {
+    Red,
+    Blue,
+    Green
+}
+let backgroundColor = color.Green;
 
 
-// //=============================normal function
-// let log = function (message) {
-//     console.log(message);
-// }
-//
-// //=============================arrow function
-// let doLog = (message) => {
-//     console.log(message);
-// }
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+let alternative = (message as string).endsWith('c');
 
-// interface Point {
-//     x: number,
-//     y: number
-// }
-// let drawPoint = (point: Point) => {
-//
-// }
-//
-// drawPoint({
-//     x: 1,
-//     y: 3
-// });
 
+//=============================normal function
+let log = function (message) {
+    console.log(message);
+}
+
+//=============================arrow function
+let doLog = (message) => {
+    console.log(message);
+}
+
+interface Point {
+    x: number,
+    y: number
+}
+let drawPoint = (point: Point) => {
+
+}
+
+drawPoint({
+    x: 1,
+    y: 3
+});
 
 class Point {
-    x: number;
-    y: number;
-
+    constructor(private x?: number, private y?: number) {
+    }
     draw ()  {
         console.log('X: ' + this.x, 'Y: ' + this.y);
     }
-    getDistance (another: Point) {
-        //...
+    get X() {
+        return this.x;
     }
-
+    set X(value) {
+        if (value < 0)
+            throw new Error('Value cannot be less than 0');
+        this.x = value;
+    }
 }
-let point = new Point();
-point.y = 1;
-point.x = 2;
+let point = new Point(1,2);
+let x = point.X;
+point.X = 10;
 point.draw();
+
+import { Point } from './module';
+let point = new Point(1,2);
+point.draw();
+
+
+
+//===============The facebook functionality quiz =========
+
+import { LikeComponent } from "./like.component";
+
+let component = new LikeComponent(10, true);
+let x = component.onClick();
+// console.log(x);
